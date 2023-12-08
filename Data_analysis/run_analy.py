6778d4bb5a9ab0d4c129a8d485455cd87187b8d3
root <root@LiLinPro.>
2023-12-08 14:35:30 +0100

    change

--- Data_analysis/run_analy.py
+++ Data_analysis/run_analy.py
@@ -4,15 +4,21 @@ import os
 
 
 def get_filepath(name,classnum_name,file_name):
-    
+    '''
+    param name: name of the dataset, pep-3k or pap
+    param classnum_name: for pap dataset, binary or multiclass
+    param file_name: dev, test, train
+    '''
     current_dir = os.path.abspath(os.path.dirname(__file__))
     parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir,os.pardir))
     return os.path.abspath(os.path.join(parent_dir,name,'train-dev-test-split',classnum_name,file_name+'.csv'))
 
 
 
-pepdev_path=get_filepath('pep-3k','','dev')
+pepdev_path=get_filepath('pap','binary','train')
 print(pepdev_path)
 pepdev_data=read_data(pepdev_path)
-print(pepdev_data)
-print(type(pepdev_data))
+#print(pepdev_data)
+print(len(pepdev_data))
+
+
