0e89bdc8576ee5e316c00530cb32d14e5aef1e86
st186102 <st186102@stud.uni-stuttgart.de>
2024-01-17 21:10:51 +0100

    update evaluation

--- pep-3k_classification/pep3k_evaluation.py
+++ pep-3k_classification/pep3k_evaluation.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import f1_score, precision_score, recall_score, roc_auc_score
+from pep3k_keras_new import ClassifierNew
 from pep3k_keras import Classifier
 
 
@@ -7,13 +8,12 @@ class Evaluation:
         This class provides methods to evaluate the performance of the classifier
 
         The file was created on     Thu Jan 11th 2024
-            it was last edited on   Mon Jan 15th 2024
+            it was last edited on   Wed Jan 17th 2024
 
         @author: Miriam S.
     """
     def __init__(self, filename_train, filename_test, filename_dev,
-                 dev_pap_bin, train_pap_bin,
-                 dev_pap_multi, train_pap_multi):
+                 dev_pap_bin, train_pap_bin):
         """
         this is the constructor for the class Evaluation containing several important variables
         it calls the class Classifier to access the classifier and the corresponding predictions
@@ -22,13 +22,10 @@ class Evaluation:
         :param filename_dev: the name of the development file
         :param dev_pap_bin: the name of the development file from pap (binary) used as additional data (dev.csv)
         :param train_pap_bin: the name of the training file from pap (binary) used as additional data (train.csv)
-        :param dev_pap_multi: the name of the development file from pap (multiclass) used as additional data (dev.csv)
-        :param train_pap_multi: the name of the training file from pap (multiclass) used as additional data (train.csv)
         @author: Miriam S.
         """
-        self.classifier = Classifier(filename_train, filename_test, filename_dev,
-                                     dev_pap_bin, train_pap_bin,
-                                     dev_pap_multi, train_pap_multi)
+        self.classifier = ClassifierNew(filename_train, filename_test, filename_dev,
+                                     dev_pap_bin, train_pap_bin)
         self.true_labels = self.classifier.labels_test
         self.predicted_labels = self.classifier.predict_plausibility()
 
@@ -63,4 +60,18 @@ class Evaluation:
         @author: Miriam S.
         """
         return roc_auc_score(self.true_labels, self.predicted_labels)
+
+
+if __name__ == "__main__":
+    eval = Evaluation("../pep-3k/train-dev-test-split/train.csv",
+                      "../pep-3k/train-dev-test-split/test.csv",
+                      "../pep-3k/train-dev-test-split/dev.csv",
+                      "../binary/dev.csv",
+                      "../binary/train.csv")
+    print(eval.true_labels)
+    print(eval.predicted_labels)
+    print("prec", eval.precision())
+    print("rec", eval.recall())
+    print("f1", eval.f1_score())
+    print("auc roc", eval.roc_auc())
 
